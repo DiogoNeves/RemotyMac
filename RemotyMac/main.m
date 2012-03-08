f239b09753b1654dd0808e076f63761c3ee42451
Diogo Neves <diogo.neves@gmail.com>
2012-03-08 18:57:34 +0000

    Initial Commit

---
+++ RemotyMac/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  RemotyMac
+//
+//  Created by Diogo Neves on 08/03/2012.
+//  Copyright (c) 2012 Wildbunny Ltd. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+	return NSApplicationMain(argc, (const char **)argv);
+}
